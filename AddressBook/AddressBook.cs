d323f6cb3a4b55f4bf201012e3b596e68372e1b2
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-30 19:07:37 +0530

    [ SANDIP ] Adress book

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipCode { get; set; }
+        public int phoneNumber { get; set; }
+        public string emailID { get; set; }
+    }
+}
